80d22a3d27217be192b2d7d34f2e4e820ed5a7ae
abish <abish@DESKTOP-3RPG6K9>
2024-10-11 20:11:50 +0530

    First Commit demo

---
+++ src/gitDemoproject/newwMaininn.java
@@ -0,0 +1,10 @@
+package gitDemoproject;
+
+public class newwMaininn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
